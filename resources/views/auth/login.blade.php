1e1498e4de2f4d192d19975791770c09a0e2b059
kapufskipauw <rosarhode@gmail.com>
2026-07-03 16:58:26 +0200

    feat: Added session directive
    that displays a message when login succeded. The message will disapear after 3 seconds

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,7 +3,6 @@
             <form action="/login" method="POST" class="mt-10 space-y-4">
                 @csrf
 
-
                 <x-form.field name="email" label="Email" type="email" />
                 <x-form.field name="password" label="Password" type="password" />
 
--- resources/views/components/layout/layout.blade.php
+++ resources/views/components/layout/layout.blade.php
@@ -6,10 +6,24 @@
     <title>Idea</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
+
 <body class="bg-background text-foreground">
     <x-layout.nav />
+
     <main class="max-w-7xl mx-auto px-4">
         {{ $slot }}
     </main>
+
+
+@session('success')
+    <div
+        x-data="{ show:true }"
+        x-init="setTimeout(() => show = false, 3000)"
+        x-show="show"
+        class="bg-primary px-4 py-3 absolute bottom-4 right-4 rounded-lg">
+
+        {{ $value }}
+    </div>
+@endsession
 </body>
 </html>
